0b6287ac3c9cf3e3107a6a680b44272836e6e7c7
kasimali-dhuka <kasimalidhuka@gmail.com>
2021-05-03 13:50:35 +0530

    rss-feed completed

--- taglink-clone/js/feed-req.js
+++ taglink-clone/js/feed-req.js
@@ -27,34 +27,38 @@ $(document).ready(function(){
     // });
 
     
-    $("#url").on("keyup", function(event){
-        if(event.keyCode == 86 || event.keyCode == 13){
-                var req = {
-                    url: $("#url").val()
-                };
+    $("#btn").on("click", function(){
+        var req = {
+            url: $("#url").val()
+        };
 
-                var response = $.ajax({
-                    url: "http://127.0.0.1:8800/get_feed/",
-                    type: "POST",
-                    data: JSON.stringify(req),
-                    contentType: "application/json",
-                    dataType: "json"
-                });
+        var response = $.ajax({
+            url: "http://127.0.0.1:8800/get_feed/",
+            type: "POST",
+            data: JSON.stringify(req),
+            contentType: "application/json",
+            dataType: "json"
+        });
 
-                response.done(function(data){
-                    // $("#taglink-model").addClass("show");
-                    // $("#title").text(data.page.title);
-                    // if(data.og.image){
-                    //     $("#img").attr("src", data.og.image);
-                    // }else{
-                    //     $("#img").attr("src", "images/github.png");
-                    // }
-                    // $("#description").text(data.meta.description);
-                    console.log(data.entries[2]);
-                });
-        }else{
-            $("#taglink-model").removeClass("show");
-        }
+        response.done(function(data){
+            if(data.entries){
+                for(var i = 0; i < 5; i++){
+                    let head = $("<li></li>").html(
+                        `
+                        <h4> ${data.entries[i].title} </h4>
+                        <p> ${data.entries[i].summary} </p>
+                        <a href=${data.entries[i].link} target='_blank'>
+                            Visit from here &nbsp;
+                            <i class="fas fa-arrow-right"></i>
+                        </a>
+                        <span>${data.entries[i].published}</span>
+                        `
+                    );
+                    $("#ol-list").append(head);
+                }
+            }
+            console.log(data.entries[2]);
+        });
     });
     
 });
